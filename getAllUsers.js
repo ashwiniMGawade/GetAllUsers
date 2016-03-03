616ca512493e99779751cfebfc6f87220c9bed17
ashwinimg-cuelogic <ashwini.gawade@cuelogic.co.in>
2016-03-03 18:13:52 +0530

    #comment initial setup with the default folder structure and package.json file

---
+++ getAllUsers.js
@@ -0,0 +1,12 @@
+console.log('Loading function');
+
+exports.handler = function(event, context) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    /*event.Records.forEach(function(record) {
+     console.log(record.eventID);
+     console.log(record.eventName);
+     console.log('DynamoDB Record: %j', record.dynamodb);
+     });
+     context.succeed("Successfully processed " + event.Records.length + " records.");*/
+    context.done(null, {"test" :  JSON.stringify(event, null, 2)});
+};
